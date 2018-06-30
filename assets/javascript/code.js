77c2e4b571a30e723bb63ba8c2598df56b5fa064
Castracani <willmyers993@gmail.com>
2018-06-30 11:01:30 -0400

    linking and initializing Firebase

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,5 +1,17 @@
-<<<<<<< HEAD
- 
-=======
 // steam api key 597FC535B0A81C139B5227A808EAA15B
->>>>>>> 32772e71e69651ac8039d8e648156472d08754a6
+
+$(document).ready(function() {
+
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBZvq3N02QOEG6fa-6qiDLwOHoSeIBza0Q",
+  authDomain: "game-website-9813e.firebaseapp.com",
+  databaseURL: "https://game-website-9813e.firebaseio.com",
+  projectId: "game-website-9813e",
+  storageBucket: "game-website-9813e.appspot.com",
+  messagingSenderId: "70919236665"
+};
+firebase.initializeApp(config);
+
+});
