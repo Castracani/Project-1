94eaf30032100d5dfb4f1d425d377cfaaf808969
smiley-coyote <nick.a.alex@gmail.com>
2018-07-02 22:56:36 -0400

    added jordan's carousel code

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,87 +1,91 @@
 
-// Steam api =========================================================================================================================
-function getSteamId(steamId) {
-   // Fetch steam user ID number
-   var queryURL = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=597FC535B0A81C139B5227A808EAA15B&vanityurl=" + steamId
-   $.ajax({
-      url: queryURL,
-      method: "GET"
-   }).then(function (response) {
-      steamNumber = response.steamid;
-      getSteamProfile(steamNumber);
-   })
-}
-$(document).on("click", "steam-id", function (event) {
-   event.prevendDefault();
-   var steamId = $("#steam-id-input").val().trim();
-   addGames(steamId);
-})
+
+$(document).ready(function () {
 
 
-// Fetch steam user profile page
-
-      // nick's steam ID for testing purposes: 76561197972752173
-
-function getSteamProfile(steamNumber) {
-   var queryURL = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=597FC535B0A81C139B5227A808EAA15B&steamids=" + steamNumber;
-   $.ajax({
-      url: queryURL,
-      method: "GET"
-   }).then(function (response) {
-
-   })
-}
-// ==================================================================================================================================
-
-// xboxAPI key 40687e73c58e72dd8d225be86a8a11de96b04dda
-
-// gamebomb API start =================================================================>>>
-var gameLibrary = [];
-var searchResults = [];
-
-function addGames(game) {
-
-   var queryURL = "https://www.giantbomb.com/api/search/?api_key=47d89cf2776025d8ace3e66e641a4eb8bd066fc5&format=json&query=" + game + "&resources=game&format=json"
-   $.ajax({
-      url: queryURL,
-      method: "GET"
-   }).then(function (response) {
-      console.log(response)
-      // create a new list for the upcoming results
-      var newList = $("<ul>");
-      ("#game-div").append(newList);
-      // clear searchResults array
-      searchResults = [""];
-      //  cycle through results
-      for (i = 0; i < response.length; i++) {
-         var newButton = $("<button>" + "add to library" + "</button");
-         newButton.addClass("add-game");
-         newButton.val([i]);
-         var list = $("<li>");
-         var gameName = $("<h1>").text(response.name);
-         var gameImage = $("<img>").attr("src", response.thumb_url);
-         var gameInfo = $("<p>").text(response.deck);
-
-         // push game title to search array
-         searchResults.push(gameName);
-
-         // prints game name, game image, game info, and an 'add to library' button to DIV
-         $("#game-div").append(list, gameName, gameImage, gameInfo, newButton);
-      }
-
-   })
-
-   // game search input+++++++++++++++++++++++++++++++++++++++++
-
-   $(document).on("click", "add-game", function (event) {
+   // Steam api =========================================================================================================================
+   function getSteamId(steamId) {
+      // Fetch steam user ID number
+      var queryURL = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=597FC535B0A81C139B5227A808EAA15B&vanityurl=" + steamId
+      $.ajax({
+         url: queryURL,
+         method: "GET"
+      }).then(function (response) {
+         steamNumber = response.steamid;
+         getSteamProfile(steamNumber);
+      })
+   }
+   $(document).on("click", "steam-id", function (event) {
       event.prevendDefault();
-      var inputGame = $("#game-input").val().trim();
-      addGames(inputGame);
+      var steamId = $("#steam-id-input").val().trim();
+      addGames(steamId);
    })
-}
 
-// add game to library
-   $(document).on("click", ".add-game", function(){
+
+   // Fetch steam user profile page
+
+   // nick's steam ID for testing purposes: 76561197972752173
+
+   function getSteamProfile(steamNumber) {
+      var queryURL = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=597FC535B0A81C139B5227A808EAA15B&steamids=" + steamNumber;
+      $.ajax({
+         url: queryURL,
+         method: "GET"
+      }).then(function (response) {
+
+      })
+   }
+   // ==================================================================================================================================
+
+   // xboxAPI key 40687e73c58e72dd8d225be86a8a11de96b04dda
+
+   // giantbomb API start =================================================================>>>
+   var gameLibrary = [];
+   var searchResults = [];
+
+   function addGames(game) {
+
+      var queryURL = "https://www.giantbomb.com/api/search/?api_key=47d89cf2776025d8ace3e66e641a4eb8bd066fc5&format=json&query=" + game + "&resources=game&format=json"
+      $.ajax({
+         url: queryURL,
+         method: "GET"
+      }).then(function (response) {
+         console.log(response)
+         // create a new list for the upcoming results
+         var newList = $("<ul>");
+         ("#game-div").append(newList);
+         // clear searchResults array
+         searchResults = [""];
+         //  cycle through results
+         for (i = 0; i < response.length; i++) {
+            var newButton = $("<button>" + "add to library" + "</button");
+            newButton.addClass("add-game");
+            newButton.val([i]);
+            var list = $("<li>");
+            var gameName = $("<h1>").text(response.name);
+            var gameImage = $("<img>").attr("src", response.thumb_url);
+            var gameInfo = $("<p>").text(response.deck);
+
+            // push game title to search array
+            searchResults.push(gameName);
+
+            // prints game name, game image, game info, and an 'add to library' button to DIV
+            $("#game-div").append(list, gameName, gameImage, gameInfo, newButton);
+         }
+
+      })
+
+      // game search input+++++++++++++++++++++++++++++++++++++++++
+
+      $(document).on("click", "add-game", function (event) {
+         event.prevendDefault();
+         var inputGame = $("#game-input").val().trim();
+         addGames(inputGame);
+      })
+   }
+
+   // add game to library
+   $(document).on("click", ".add-game", function () {
       var x = $(this).val();
       var newGame = searchResults[x];
       console.log(newGame);
@@ -92,34 +96,46 @@ function addGames(game) {
    })
 
 
-// giantbomb API end <<<=================================================================
+   // giantbomb API end <<<=================================================================
 
 
-// Profile creation start ============================================================>>>
+   // Profile creation start ============================================================>>>
 
-// object that will hold user profile locally
-var curUser = {
-   name: "",
-   avatar: "",
-   steamName: "",
-   psnName: "",
-   xboxName: "",
-   nintendoId: "",
-   aboutMe: "",
-   status: "",
-   // will add more later
-}
+   // object that will hold user profile locally
+   var curUser = {
+      name: "",
+      avatar: "",
+      steamName: "",
+      psnName: "",
+      xboxName: "",
+      nintendoId: "",
+      aboutMe: "",
+      status: "",
+      // will add more later
+   }
 
-$(document).on("click", "#user-submit", function () {
-   curUser.name = ("#name-input").val().trim();
-   curUser.steamName = ("#steam-input").val().trim();
-   curUser.psnName = ("#psn-input").val().trim();
-   curUser.xboxName = ("#xbox-input").val().trim();
-   curUser.nintendoId = ("#nintendo-input").val().trim();
-   curUser.aboutMe = ("#about-input").val().trim();
+   $(document).on("click", "#user-submit", function () {
+      curUser.name = ("#name-input").val().trim();
+      curUser.steamName = ("#steam-input").val().trim();
+      curUser.psnName = ("#psn-input").val().trim();
+      curUser.xboxName = ("#xbox-input").val().trim();
+      curUser.nintendoId = ("#nintendo-input").val().trim();
+      curUser.aboutMe = ("#about-input").val().trim();
 
-   // print and format data to DIV containing user info on profile page
-   // store data into firebase
+      // print and format data to DIV containing user info on profile page
+      // store data into firebase
+   })
+
+   $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true,
+      next: 3
+   });
+
+   setInterval(function () {
+      $('.carousel').carousel('next');
+   }, 5000);
+
 })
 
 // Profile creation end <<<=================================================================
@@ -131,8 +147,8 @@ $(document).on("click", "#user-submit", function () {
 
 /*
 
-$(document).ready(function () {
-   $('.carousel').carousel();
+
+   
 
    // Initialize Firebase
    var config = {
@@ -145,7 +161,7 @@ $(document).ready(function () {
    };
    firebase.initializeApp(config);
 
-});
+
 
 */
 
